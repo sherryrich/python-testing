950fad9b644b382074e9b52ff91567a71a023aae
sherryrich <ricsherry@gmail.com>
2022-04-16 11:20:36 +0000

    testing

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-print("hello")
+# print("hello")
 # input() will get user to input in console
 # Then print() will print the word "hello" and the user input
-print("Hello " + input("what is your name?"))
+# print("Hello " + input("what is your name?"))# stops running until user enters input
+# print("Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+# prints the number of characters in name
+# print( len( input("What is your name?") ) ) 
+# variables
+# name = input("what is your name?")
+# print(name)
+
+# name = "jack"
+# print(name)
+
+# name = "Peter"
+# print(name)
+
+# name = input("what is your name?")
+# lenght = len(name)
+# print(lenght)
