e156c855557709f9e148fe1b626d20a54188d046
sherryrich <ricsherry@gmail.com>
2022-04-20 19:59:18 +0000

    updated nested lists

---
+++ nested_lists.py
@@ -0,0 +1,15 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# print(states_of_america)
+# print(states_of_america[49])
+# print(states_of_america[50]) # IndexError: list index out of range
+
+# num_of_states = len(states_of_america)
+# print(states_of_america[num_of_states -1])
+
+#combining 2 lists
+# fruits = ["strawberries", "apples", "grapes"]
+# print(fruits)
+# vegetables = ["spinach", "tomatoes", "potatoes"]
+# print(vegetables)
+# combined = [fruits, vegetables]
+# print(combined)
