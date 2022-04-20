a7aedde06f4642dfd1be4e652ba261ae9bba2fdb
sherryrich <ricsherry@gmail.com>
2022-04-20 19:50:07 +0000

    who pays completed

--- whopays.py
+++ whopays.py
@@ -1,9 +1,13 @@
 # import random
-# # Split string method
+# # # Split string method
 # names_string = input("Give me everybody's names, separated by a comma. ")
 # names = names_string.split(", ")
-# num_items = len(names)
-# random_choice = random.randint(0, num_items -1)
-# person_who_will_pay = names[random_choice]
-# print(person_who_will_pay + " is going to pay today")
+# # num_items = len(names)
+# # random_choice = random.randint(0, num_items -1)
+# # person_who_will_pay = names[random_choice]
+# # print(person_who_will_pay + " is going to pay today")
 
+# # alt option with less code
+
+# person_who_will_pay = random.choice(names)
+# print(person_who_will_pay + " is going to bu the meal today")
