e871ec920eaf1a3e6963d759f43fd5cd0fb69998
sherryrich <ricsherry@gmail.com>
2022-04-20 19:45:52 +0000

    who pays random

---
+++ lists.py
@@ -0,0 +1,17 @@
+# fruits = ["apples", "pears"]
+# print(fruits)
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# print(states_of_america)
+print(states_of_america[0])
+print(states_of_america[3])
+
+# to add an item at the end of list - 
+
+# NB: https://docs.python.org/3/tutorial/datastructures.html
+
+# states_of_america.append("Richard_Land")
+# print(states_of_america)
+
+states_of_america.extend(["russia, alaska"]) # extend by 2 more items
+print(states_of_america)
---
+++ whopays.py
@@ -0,0 +1,9 @@
+# import random
+# # Split string method
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# num_items = len(names)
+# random_choice = random.randint(0, num_items -1)
+# person_who_will_pay = names[random_choice]
+# print(person_who_will_pay + " is going to pay today")
+
