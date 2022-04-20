f71f85e6ca8729d154dc020e7b8482504ba96abb
sherryrich <ricsherry@gmail.com>
2022-04-20 19:12:15 +0000

    random heads or tails

--- randomisation.py
+++ randomisation.py
@@ -1,7 +1,22 @@
 import random
-import my_module # imported different file
+# import my_module # imported different file
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+# random_integer = random.randint(1, 10)
+# print(random_integer)
 
-print(my_module.pi) # reference different file
+# print(my_module.pi) # reference different file
+
+# random_float = random.random() * 5 # multiple by 5 to bring the decimal to 1-5
+# print(random_float)
+
+# random number between 1 and 100
+
+# score = random.randint(1, 100)
+# print(f"Random number is {score}")
+
+# RANDOM HEADS OR TAILS
+# random_side = random.randint(0, 1)
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
