e8bc81acd81d6f308dd6b8dc68e5a26695ee1f32
sherryrich <ricsherry@gmail.com>
2022-04-21 10:37:28 +0000

    even numbers exercise

--- loops.py
+++ loops.py
@@ -40,3 +40,24 @@
 #     if score > highest_score:
 #         highest_score = score
 # print(f"The highest score in the clsee is: {highest_score}")
+
+# using for loops with range fuction, generate a range of numbers to loop through
+
+# for number in range(1, 100):
+#     print(number) # prints no 1 - 100
+
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
+
+# total = 0
+# for number in range(2, 101, 2): #change start of range to 2 and step size to 2
+#     total += number
+# print(number)
+# total2 = 0
+# for number in range(1, 101):
+#     if number % 2 == 0:
+#         total2 += number
+
+# print(total2)
