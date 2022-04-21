4455d6d122cdfb74fe52545f61b4e10ec24fd6d3
sherryrich <ricsherry@gmail.com>
2022-04-21 15:25:02 +0000

    list methods

---
+++ list_methods.py
@@ -0,0 +1,38 @@
+fruits = ['apple', 'orange', 'banana', 'pear', 'plum']
+
+# Print all fruits
+for fruit in fruits:
+    print(fruit)
+
+print()
+
+# Get an item located in a list
+second_item = fruits[1]
+print(second_item)
+print()
+
+# Add an item to the list
+fruits.append('cherries')
+print(fruits)
+print()
+
+# Reverse the list
+fruits.reverse()
+print(fruits)
+
+# Sort the list alphabetically:
+fruits.sort()
+print(fruits)
+
+menu = ['eggs', 'bacon', 'spam', 'spam']
+print(menu)
+print(menu.count('spam'))
+print(menu.count('eggs'))
+print(menu.index('eggs'))
+print(menu.reverse())
+print(menu)
+print(menu.append('lobster thermidor'))
+print(menu)
+print(menu.sort())
+print(menu)
+print(menu.pop())
