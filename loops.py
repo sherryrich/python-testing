bc9e05dcc59b2876c32a5c2244467418d4fbd07d
sherryrich <ricsherry@gmail.com>
2022-04-21 10:21:40 +0000

    highest score

--- loops.py
+++ loops.py
@@ -4,9 +4,9 @@
 #   print(fruit + " pie")
 
 # AVERAGE HEIGHT
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
 #   print(student_heights)
 #   print(len(student_heights))
 
@@ -16,15 +16,27 @@ for n in range(0, len(student_heights)):
 # average_height = round(total_height / number_of_students)
 # print(average_height)
 
-total_height = 0
-for height in student_heights:
-    total_height += height
-print(total_height)
+# total_height = 0
+# for height in student_heights:
+#     total_height += height
+# print(total_height)
 
-number_of_students = 0
-for student in student_heights:
-    number_of_students += 1
-print(number_of_students)
+# number_of_students = 0
+# for student in student_heights:
+#     number_of_students += 1
+# print(number_of_students)
 
-average_height = round(total_height / number_of_students)
-print(average_height)
+# average_height = round(total_height / number_of_students)
+# print(average_height)
+
+# How to output the highest score from  a list of scores
+# student_scores = input("Input a list of student scores ").split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+# print(student_scores)
+
+# highest_score = 0
+# for score in student_scores:
+#     if score > highest_score:
+#         highest_score = score
+# print(f"The highest score in the clsee is: {highest_score}")
