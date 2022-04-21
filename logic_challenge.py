3e0967d0cbfe41de9998c07a1a175dc7a18431ef
sherryrich <ricsherry@gmail.com>
2022-04-21 07:59:36 +0000

    multiple conditions(if/elif/else)

--- logic_challenge.py
+++ logic_challenge.py
@@ -22,3 +22,24 @@
 #     print("Buzz")
 # else:
 #     print(num)
+
+#Multiple Conditions (If/Elif/Else)
+# number = int(input("Please enter a number:"))
+
+# if number > 5:
+#     print(f"{number} is greater than 5")
+# elif number < 5:
+#     print(f"{number} is less than 5")
+# else:
+#     print(f"{number} is not greater than, or less than 5. Therefore, {number} must be equal to 5.")
+
+# day = 'Friday'
+
+# if day == "Monday":
+#     print("Meeting at 9:00")
+# elif day == "Wednesday":
+#     print("Meeting at 2:00")
+# elif day == "Friday":
+#     print("Meeting at 4:00")
+# else:
+#     print("No meetings today")
