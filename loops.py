46b6213d7e24faeff608bd9ccc5a7e59ba73196c
sherryrich <ricsherry@gmail.com>
2022-04-21 10:43:36 +0000

    FizzBuzz

--- loops.py
+++ loops.py
@@ -61,3 +61,16 @@
 #         total2 += number
 
 # print(total2)
+
+
+# FizzBuzz Job Interview Question
+
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
