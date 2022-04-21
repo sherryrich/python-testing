b1101865978335675a4d6eaa660cec56bcb9c793
sherryrich <ricsherry@gmail.com>
2022-04-21 07:51:29 +0000

    FizzBuzz

---
+++ logic_challenge.py
@@ -0,0 +1,24 @@
+# number = int(input("Please enter a number:"))
+
+# if number == 5:
+#     print(f"{number} is equal to 5")
+# else:
+#     print(f"{number} is not equal to 5")
+
+
+# Challenge
+# if the number is divisible by 3 = say "Fizz"
+# if the number is divisible by 5 = say "Buzz"
+# if the number is divisible by 3 and 5 = say "FizzBuzz"
+# Otherwise say the original number
+
+# num = 1
+
+# if num % 3 == 0 and num % 5 == 0:
+#     print("FizzBuzz")
+# elif num % 3 == 0:
+#     print("Fizz")
+# elif num % 5 == 0:
+#     print("Buzz")
+# else:
+#     print(num)
