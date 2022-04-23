3633ae5fe0f1e7e8a92554632959c62d98d37113
sherryrich <ricsherry@gmail.com>
2022-04-23 11:44:32 +0000

    hangman testing

--- hangman.py
+++ hangman.py
@@ -1,17 +1,42 @@
-#Step 1 
-
-word_list = ["aardvark", "baboon", "camel"]
-
-#1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 import random
+from hangman_art import stages, logo
+from hangman_words import word_list
+
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
+
 chosen_word = random.choice(word_list)
-print(chosen_word)
-#2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter:").lower()
-print(guess)
-#3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for letter in chosen_word:
-  if letter == guess:
-    print("Match")
-  else:
-    print("wrong")
+word_length = len(chosen_word)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not game_is_finished:
+    guess = input("Guess a letter: ").lower()
+
+    #Use the clear() function imported from replit to clear the output between guesses.
+    # clear()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+    
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
+
+    print(stages[lives])
